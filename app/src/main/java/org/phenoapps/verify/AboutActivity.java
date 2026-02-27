9e40f2a9751265bf6c07bde8ec7ddae0b7b09146
trife <twrife@clemson.edu>
2026-02-27 14:34:56 -0500

    fix more icons and about fragment

--- app/src/main/java/org/phenoapps/verify/AboutActivity.java
+++ app/src/main/java/org/phenoapps/verify/AboutActivity.java
@@ -1,109 +1,54 @@
 package org.phenoapps.verify;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import android.os.Bundle;
-
-import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
-import com.danielstone.materialaboutlibrary.ConvenienceBuilder;
-import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
-import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
-import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
-import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
-
-import android.content.Context;
-import android.net.Uri;
+import android.view.MenuItem;
 import android.view.View;
 
-import androidx.core.content.ContextCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class AboutActivity extends MaterialAboutActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.phenoapps.verify.utilities.InsetHandler;
+
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        // Apply bottom/side insets for edge-to-edge (MaterialAboutActivity handles the toolbar top inset)
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.about_toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getString(R.string.about));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        BottomNavigationView bnv = findViewById(R.id.about_bnv);
+        // No item pre-selected in About
+        bnv.getMenu().setGroupCheckable(0, false, true);
+
         View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
-        androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
-            androidx.core.graphics.Insets bars = insets.getInsets(
-                    androidx.core.view.WindowInsetsCompat.Type.systemBars() |
-                    androidx.core.view.WindowInsetsCompat.Type.displayCutout());
-            v.setPadding(bars.left, 0, bars.right, bars.bottom);
-            return insets;
+        InsetHandler.INSTANCE.setupInsetsWithBottomNav(rootView, toolbar, bnv);
+
+        bnv.setOnItemSelectedListener(item -> {
+            finish();
+            return true;
         });
-        androidx.core.view.ViewCompat.requestApplyInsets(rootView);
     }
 
-    @NonNull
     @Override
-    protected MaterialAboutList getMaterialAboutList(@NonNull Context context) {
-
-        MaterialAboutCard.Builder appCardBuilder = new MaterialAboutCard.Builder();
-
-        appCardBuilder.addItem(new MaterialAboutTitleItem.Builder()
-                .text(getString(R.string.app_name))
-                .icon(R.mipmap.ic_launcher)
-                .build());
-
-        appCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(this,
-                ContextCompat.getDrawable(this, R.drawable.ic_about),
-                "Version",
-                false));
-
-        appCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context,
-                ContextCompat.getDrawable(this, R.drawable.ic_about),
-                "GitHub",
-                false,
-                Uri.parse("https://github.com/PhenoApps/Verify")));
-
-        MaterialAboutCard.Builder authorCardBuilder = new MaterialAboutCard.Builder();
-        authorCardBuilder.title("Developers");
-
-        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-                .text(getString(R.string.dev_chaney))
-                .subText(getString(R.string.ksu))
-                .icon(R.drawable.ic_person_profile)
-                .build());
-
-        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-                .text(getString(R.string.dev_trevor))
-                .subText(getString(R.string.ksu) + "\n" + getString(R.string.dev_trevor_email))
-                .icon(R.drawable.ic_person_profile)
-                .build());
-
-        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-                .text(getString(R.string.dev_jesse))
-                .subText(getString(R.string.ksu) + "\n" + getString(R.string.dev_jesse_email))
-                .icon(R.drawable.ic_person_profile)
-                .build());
-
-        MaterialAboutCard.Builder descriptionCard = new MaterialAboutCard.Builder();
-        descriptionCard.title("Description");
-        descriptionCard.addItem(new MaterialAboutActionItem.Builder()
-                .text("Verify imports a list of entries, scans barcodes, and identifies whether " +
-                        "each entry exists in the list with audio/visual notifications.")
-                .build());
-
-        MaterialAboutCard.Builder otherAppsCard = new MaterialAboutCard.Builder();
-        otherAppsCard.title("PhenoApps");
-        otherAppsCard.addItem(ConvenienceBuilder.createWebsiteActionItem(context,
-                ContextCompat.getDrawable(this, R.drawable.ic_about),
-                "PhenoApps.org",
-                false,
-                Uri.parse("http://phenoapps.org/")));
-
-        return new MaterialAboutList(
-                appCardBuilder.build(),
-                authorCardBuilder.build(),
-                descriptionCard.build(),
-                otherAppsCard.build());
-    }
-
-    @Nullable
-    @Override
-    protected CharSequence getActivityTitle() {
-        return getString(R.string.about);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
 
